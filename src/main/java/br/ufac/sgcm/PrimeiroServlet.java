1ae527d7082834f0315d296b7815fe5e7d92cd27
Wallison Ferreira <contato.sanfer@gmail.com>
2022-09-01 19:02:40 -0500

    first commit

---
+++ src/main/java/br/ufac/sgcm/PrimeiroServlet.java
@@ -0,0 +1,16 @@
+package br.ufac.sgcm;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+
+public class PrimeiroServlet extends HttpServlet{
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.service(req, res);
+    }
+}
